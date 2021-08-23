add3cf6fa969ae967da7226e3722267c327c24d5
Griffin Schrauth <gschrauth2018@fau.edu>
2021-08-23 15:16:06 -0400

    setup completer.looks like gameboard now

--- connect4/connect4.js
+++ connect4/connect4.js
@@ -1,6 +1,14 @@
 const boxes = Array.from(document.getElementsByClassName('box'));
 console.log(boxes);
 
+const playText = document.getElementById('playText');
+const restartBtn = document.getElementById('restartBtn');
+const spaces = [];
+const O_TEXT = "O";
+const X_TEXT = "X";
+let currentPlayer;
+
+
 const drawBoard = () => {
     boxes.forEach((box,index) => {
         let styleString = '';
@@ -14,8 +22,59 @@ const drawBoard = () => {
             styleString += 'border-top: 3px solid var(--purple);';
         }
         box.style = styleString;
-        //box.addEventListener('click',boxClicked)
+        box.addEventListener('click',boxClicked)
 
     });
 };
+
+const boxClicked = (e) => {
+    
+    const id = e.target.id;
+   
+    if(!spaces[id]){
+        spaces[id] = currentPlayer;
+        e.target.innerText = currentPlayer;
+        if(playerHasWon()){
+            playText.innerText = `${currentPlayer} has won`;
+            return;
+        }
+        currentPlayer = currentPlayer == O_TEXT ? X_TEXT : O_TEXT;
+    }
+
+};
+
+const playerHasWon = () => {
+    if(spaces[0] == currentPlayer){
+        if(spaces[1] == currentPlayer && spaces[2] == currentPlayer){
+            console.log(`${currentPlayer} wins up top`)
+            return true;
+        }
+        if(spaces[3] == currentPlayer && spaces[6] == currentPlayer){
+            console.log(`${currentPlayer} wins on the left`)
+            return true;
+        }
+        if(spaces[4] == currentPlayer && spaces[8] == currentPlayer){
+            console.log(`${currentPlayer} wins diagonally`)
+            return true;
+        }
+        
+    }
+   
+};
+
+const restart = () => {
+    spaces.forEach((space,index) => {
+        spaces[index] = null; 
+    })
+    boxes.forEach(box => {
+        box.innerText = '';
+    })
+    playText.innerText = `Let's Play!`;
+    currentPlayer = O_TEXT;
+}
+
+
+restartBtn.addEventListener('click', restart);
+restart();
+
 drawBoard();
